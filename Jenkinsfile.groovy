3fc983bf6bf51883f0244e5236a998ae15c03985
Harshith <97779040+harshith010@users.noreply.github.com>
2024-04-10 19:41:50 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Running tests'
-                app()
+                hlpTrial.app()
                 // Add test execution steps here
             }
         }
